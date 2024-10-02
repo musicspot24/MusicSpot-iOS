8e2029c9a3133546813bd89e8d7cc4c9a9887ee1
Junnos  <junnos.dev@gmail.com>
2024-10-02 14:18:44 +0900

    ✨ Countdown 추가

--- iOS/MusicSpot/MSCoreKit/Sources/Tickr/Countdown.swift
+++ iOS/MusicSpot/MSCoreKit/Sources/Tickr/Countdown.swift
@@ -7,10 +7,62 @@
 
 import Foundation
 
-public struct Countdown<C: Clock> {
-    let clock: C
+public struct Countdown<C: Clock>: AsyncSequence, Sendable {
 
-    public init(clock: C) {
+    // MARK: Nested Types
+
+    public struct CountdownItrator: AsyncIteratorProtocol {
+
+        // MARK: Properties
+
+        let clock: C
+        let duration: C.Duration
+        let epochs: Int
+        var currentEpoch: Int = .zero
+
+        // MARK: Lifecycle
+
+        init(clock: C, duration: C.Duration, epochs: Int) {
+            self.clock = clock
+            self.duration = duration
+            self.epochs = epochs
+        }
+
+        // MARK: Functions
+
+        public mutating func next() async -> C.Instant? {
+            defer { currentEpoch += 1 }
+            if currentEpoch >= epochs {
+                return nil
+            }
+
+            do {
+                try await clock.sleep(for: duration)
+            } catch {
+                return nil
+            }
+
+            return clock.now
+        }
+    }
+
+    // MARK: Properties
+
+    let clock: C
+    let duration: C.Duration
+    let epochs: Int
+
+    // MARK: Lifecycle
+
+    public init(clock: C, duration: C.Duration, repeat epochs: Int) {
         self.clock = clock
+        self.duration = duration
+        self.epochs = epochs
+    }
+
+    // MARK: Functions
+
+    public func makeAsyncIterator() -> CountdownItrator {
+        CountdownItrator(clock: clock, duration: duration, epochs: epochs)
     }
 }
--- iOS/MusicSpot/MSCoreKit/Tests/TickrTests/CountdownTests.swift
+++ iOS/MusicSpot/MSCoreKit/Tests/TickrTests/CountdownTests.swift
@@ -5,17 +5,68 @@
 //  Created by 이창준 on 9/28/24.
 //
 
+import OSLog
 import Testing
 
 import Tickr
 
 struct CountdownTests {
+
+    // MARK: Properties
+
     let clock: SuspendingClock
+
+    // MARK: Lifecycle
 
     init() async throws {
         clock = .suspending
     }
 
-    @Test
-    func example() async throws { }
+    // MARK: Functions
+
+    @Test("Countdown using sequence and for loop", arguments: [3, 5, 10])
+    func countdownWithSequence(iteration: Int) async throws {
+        var count: Int = .zero
+
+        let countdown = clock.countdown(duration: .seconds(2), repeat: iteration)
+        await confirmation(expectedCount: iteration) { confirmation in
+            for await _ in countdown {
+                count += 1
+                confirmation.confirm()
+            }
+        }
+
+        #expect(count == iteration)
+    }
+
+    @Test("Countdown with no yielded value", arguments: [3, 5, 10])
+    func countdownWithNoYield(iteration: Int) async throws {
+        var count: Int = .zero
+
+        await confirmation(expectedCount: iteration) { confirmation in
+            await clock.countdown(from: .seconds(2), repeat: iteration) {
+                count += 1
+                confirmation.confirm()
+            }
+        }
+
+        #expect(count == iteration)
+    }
+
+    @Test("Countdown with yielded value", arguments: [3, 5, 10])
+    func countdownWithYield(repeat: Int) async throws {
+        let countdown = clock.countdown(from: .seconds(2), repeat: `repeat`, initialElement: 0) { partialResult in
+            partialResult + 2
+        }
+
+        var count: Int = .zero
+        await confirmation(expectedCount: `repeat`) { confirmation in
+            for await current in countdown {
+                count += 1
+                Logger().info("Current: \(current), Count: \(count)")
+                #expect(current == count * 2)
+                confirmation.confirm()
+            }
+        }
+    }
 }
